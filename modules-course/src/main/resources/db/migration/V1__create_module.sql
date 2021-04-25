dd8ed7d1ead15950fbb84431ad1d04da4a0df756
andferreira <anderson.ferreira@mercadolivre.com>
2021-04-25 14:33:09 -0300

    Add settings to access database

---
+++ modules-course/src/main/resources/db/migration/V1__create_module.sql
@@ -0,0 +1,4 @@
+create table module (
+    id int not null auto_increment primary key,
+    name varchar(80) not null
+);
---
+++ modules-course/src/main/resources/db/migration/V2__insert_module.sql
@@ -0,0 +1,5 @@
+insert into module (name) values ('Docker');
+insert into module (name) values ('Service Discovery');
+insert into module (name) values ('Kubernetes');
+insert into module (name) values ('Rabbit MQ');
+insert into module (name) values ('Kafka');
