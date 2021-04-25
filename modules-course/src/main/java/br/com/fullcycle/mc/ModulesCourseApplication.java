ba7acb18db003564ceb75a6f2e61d16e8b404b87
andferreira <anderson.ferreira@mercadolivre.com>
2021-04-25 13:09:44 -0300

    up new struct of the project

---
+++ modules-course/src/main/java/br/com/fullcycle/mc/ModulesCourseApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullcycle.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulesCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulesCourseApplication.class, args);
+	}
+
+}
---
+++ modules-course/src/test/java/br/com/fullcycle/mc/ModulesCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullcycle.mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulesCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
